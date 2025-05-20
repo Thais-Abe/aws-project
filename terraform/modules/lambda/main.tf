4d22ade9add429f1ba0d5776db51a4a54a241200
Thais-Abe <abe.c.thais@gmail.com>
2025-05-20 14:17:30 -0300

    add .zip automatic

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,3 +1,12 @@
+data "archive_file" "lambda_zips" {
+  for_each    = var.lambda_configs
+  type        = "zip"
+  source_dir  = each.value.source_path
+  output_path = "${path.module}/zip/${each.value.function_name}.zip"
+}
+
+
+
 # Cria uma Lambda para cada configuração
 resource "aws_lambda_function" "lambda_functions" {
   for_each = var.lambda_configs
@@ -5,8 +14,8 @@ resource "aws_lambda_function" "lambda_functions" {
   function_name    = each.value.function_name
   handler          = each.value.handler
   runtime          = each.value.runtime
-  filename         = each.value.filename
-  source_code_hash = filebase64sha256(each.value.filename)
+  filename         = data.archive_file.lambda_zips[each.key].output_path
+  source_code_hash = data.archive_file.lambda_zips[each.key].output_base64sha256
   role             = var.lambda_role_arn
 
   environment {
@@ -16,3 +25,5 @@ resource "aws_lambda_function" "lambda_functions" {
   timeout     = each.value.timeout
   memory_size = each.value.memory_size
 }
+
+
