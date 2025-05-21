f842afab03d4e5c680f3eb2b28529c0c4396a76c
cibelipetrone <cibeli.petrone@gmail.com>
2025-05-21 15:25:02 -0300

    feat: add module apigateway to main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,14 @@ module "cognito" {
   source             = "./terraform/modules/cognito"
   user_pool_name     = var.user_pool_name
 }
+
+
+module "api_gateway" {
+  source = "./terraform/modules/apigateway"
+
+  lambda_arn           = module.lambda_functions.lambda_arns["lambda_hello"]
+  lambda_name          = module.lambda_functions.lambda_names["lambda_hello"]
+  cognito_user_pool_id = module.cognito.user_pool_id
+  cognito_app_client_id = module.cognito.app_client_id
+  region               = "sa-east-1"
+}
