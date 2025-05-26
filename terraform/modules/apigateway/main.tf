4bdf349ee81ed5d620c95100c6f2d82fd207e2bf
Thais-Abe <abe.c.thais@gmail.com>
2025-05-26 16:47:31 -0300

    feat: change integration_uri from api gateway

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -10,7 +10,7 @@ resource "aws_apigatewayv2_api" "http_api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.http_api.id
   integration_type       = "AWS_PROXY"
-  integration_uri        = var.lambda_arn
+  integration_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
   integration_method     = "POST"
   payload_format_version = "2.0"
 }
