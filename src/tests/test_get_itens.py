8edbc8cec7d51a5a12472f437d51731476cef0de
Valéria Santos <santosvaleriav4@gmail.com>
2025-05-29 13:01:32 -0300

    fix: adjusting unit tests

--- src/tests/test_get_itens.py
+++ src/tests/test_get_itens.py
@@ -9,20 +9,14 @@ from lambdas_python.lambda_get_itens import get_itens
 @pytest.fixture
 def mock_event():
     return {
-        'requestContext': {
-            'authorizer': {
-                'claims': {
-                    'sub': '123'
-                }
-            }
-        }
+        'queryStringParameters': {'date': '2025-05-14'} 
     }
 
 # teste em caso de sucesso na consulta ao DynamoDB
 def test_lambda_handler_success(mock_event):
-    mock_items = [{'PK': 'USER#123', 'SK': 'ITEM#1', 'name': 'item1'}]
+    mock_items = [{'PK': 'LIST#2025-05-14', 'SK': 'ITEM#1', 'name': 'item1'}]
     with patch.object(get_itens.TABLE, 'query', return_value={'Items': mock_items}):
-        response = get_itens.lambda_handler(mock_event, None)
+        response = get_itens.lambda_get_itens(mock_event, None)
         body = json.loads(response['body'])
         assert response['statusCode'] == 200
         assert body['count'] == 1
@@ -31,7 +25,7 @@ def test_lambda_handler_success(mock_event):
 # teste em caso de não haver itens
 def test_lambda_handler_no_items(mock_event):
     with patch.object(get_itens.TABLE, 'query', return_value={'Items': []}):
-        response = get_itens.lambda_handler(mock_event, None)
+        response = get_itens.lambda_get_itens(mock_event, None)
         body = json.loads(response['body'])
         assert response['statusCode'] == 200
         assert body['count'] == 0
@@ -40,7 +34,7 @@ def test_lambda_handler_no_items(mock_event):
 # teste em caso de exceção na consulta ao DynamoDB
 def test_lambda_handler_exception(mock_event):
     with patch.object(get_itens.TABLE, 'query', side_effect=Exception("Dynamo error")):
-        response = get_itens.lambda_handler(mock_event, None)
+        response = get_itens.lambda_get_itens(mock_event, None)
         body = json.loads(response['body'])
         assert response['statusCode'] == 500
         assert 'error' in body
